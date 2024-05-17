d0e3c5381f5db72b400df0c801a7ca7ba9498175
hubirto <hubert.rybicki.hr@gmail.com>
2024-05-17 11:15:27 +0200

    Init

---
+++ src/main/java/pl/pomoku/mainapigateway/MainApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.pomoku.mainapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApiGatewayApplication.class, args);
+    }
+
+}
